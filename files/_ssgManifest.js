0201b236ae0e439d8b75b0d3bddcd486149ea93e
Kang mison <74046080+Kang-mison@users.noreply.github.com>
2022-05-28 21:27:07 +0900

    Add files via upload

---
+++ files/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fshop\u002Ffacemask\u002F[id]","\u002Fshop\u002Fprotein\u002F[id]","\u002Fshop\u002Fsupplement\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
